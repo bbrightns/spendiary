94ec7cd8b8390693402f9e13cec98bfb5e989cfe
bbbrightns <praweenbright@gmail.com>
2026-08-17 15:36:03 +0700

    config: change default dev server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
+  server: {
+    port: 3000,
+  },
   build: {
     chunkSizeWarningLimit: 1600,
   },
