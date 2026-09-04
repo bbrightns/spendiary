7d57695f491b990b4fd2e4f8c245cec0ddc126d4
Praween Piyaprapaphan <praweenbright@gmail.com>
2026-09-04 22:49:48 +0700

    feat(icons): add DotsHorizontalIcon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -110,6 +110,16 @@ export function MinusIcon(p: IconProps) {
   )
 }
 
+export function DotsHorizontalIcon(p: IconProps) {
+  return (
+    <svg {...base} {...p}>
+      <circle cx="12" cy="12" r="1.5" fill="currentColor" stroke="none" />
+      <circle cx="19" cy="12" r="1.5" fill="currentColor" stroke="none" />
+      <circle cx="5" cy="12" r="1.5" fill="currentColor" stroke="none" />
+    </svg>
+  )
+}
+
 export function SparkleIcon(p: IconProps) {
   return (
     <svg {...base} {...p}>
